225159c5607ecb7156a61ab72b0a955c2416d87c
shinpei_yahiro <shinpei.yahiro29@gmail.com>
2025-04-17 22:42:13 +0900

    add_add関数

--- app.py
+++ app.py
@@ -8,6 +8,21 @@ import streamlit.components.v1 as components
 import random
 import openai
 import os
+from supabase import create_client, Client
+url: str = "https://pszefvosagdpzilocerq.supabase.co"
+key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBzemVmdm9zYWdkcHppbG9jZXJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ4ODU1NTIsImV4cCI6MjA2MDQ2MTU1Mn0.nRw_Ev8VGVf_PvnQZ5Lk10JPYg3jaJwUWkGCmNO03fA"
+
+supabase: Client = create_client(url, key)
+
+##add_records("url","comment")を入れると、recordsに挿入される。
+def add_records(url,comment):
+    data= {
+        "url":url,
+        "place":comment
+    }
+    response = supabase.table("records").insert(data).execute()
+    return response
+
 
 # 音楽ファイルを base64 に変換
 def get_audio_base64(file_path):
